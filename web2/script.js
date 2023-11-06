8138264186d998902aa26395637cb6cf65ddca90
Ridhwan Firdaus Nur Prasetia <86599207+oneraid@users.noreply.github.com>
2023-11-06 14:02:15 +0700

    Delete web2 directory

--- web2/script.js
+++
@@ -1,6 +0,0 @@
-const menutoggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-menutoggle.addEventListener('click', function(){
-    nav.classList.toggle('slide');
-});
