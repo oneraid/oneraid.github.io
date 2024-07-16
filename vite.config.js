850059e14602cc4ea5a044b3349aad96688c4173
oneraid <oneraid3@gmail.com>
2024-07-16 21:39:24 +0700

    reasd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
+import path from "path";
 
-// https://vitejs.dev/config/
+// Mengonversi URL ke path
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  root: ".", // Atur root ke direktori proyek
+  build: {
+    outDir: "dist", // Direktori output untuk build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"), // Path ke file index.html
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    open: true, // Membuka browser secara otomatis
+  },
+});
